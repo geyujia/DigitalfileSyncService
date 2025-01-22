bb239a2f4438b26e91328aeb038a6f95b64cb361
geyujia <16601163652@163.com>
2025-01-22 11:23:25 +0800

    #Ocr识别工具调整#

--- DigitalfileSync/DigitalfileSyncService/Form1.Designer.cs
+++ DigitalfileSync/DigitalfileSyncService/Form1.Designer.cs
@@ -33,19 +33,27 @@
             picBox = new PictureBox();
             btnSelImage = new Button();
             openFileDialog1 = new OpenFileDialog();
-            label3 = new Label();
             btnShibie = new Button();
             chkBox1 = new CheckBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            btnHtmlReg = new Button();
+            txtHtmlContent = new TextBox();
+            btnSaveHtml = new Button();
             ((System.ComponentModel.ISupportInitialize)picBox).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // txtContent
             // 
-            txtContent.Location = new Point(615, 29);
+            txtContent.Location = new Point(6, 3);
             txtContent.Multiline = true;
             txtContent.Name = "txtContent";
             txtContent.ScrollBars = ScrollBars.Vertical;
-            txtContent.Size = new Size(797, 631);
+            txtContent.Size = new Size(919, 635);
             txtContent.TabIndex = 6;
             // 
             // picBox
@@ -53,14 +61,14 @@
             picBox.BackColor = SystemColors.AppWorkspace;
             picBox.Location = new Point(12, 12);
             picBox.Name = "picBox";
-            picBox.Size = new Size(554, 685);
+            picBox.Size = new Size(554, 658);
             picBox.SizeMode = PictureBoxSizeMode.StretchImage;
             picBox.TabIndex = 7;
             picBox.TabStop = false;
             // 
             // btnSelImage
             // 
-            btnSelImage.Location = new Point(615, 666);
+            btnSelImage.Location = new Point(486, 676);
             btnSelImage.Name = "btnSelImage";
             btnSelImage.Size = new Size(80, 31);
             btnSelImage.TabIndex = 8;
@@ -72,18 +80,9 @@
             // 
             openFileDialog1.FileName = "openFileDialog1";
             // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(615, 9);
-            label3.Name = "label3";
-            label3.Size = new Size(59, 17);
-            label3.TabIndex = 9;
-            label3.Text = "识别结果:";
-            // 
             // btnShibie
             // 
-            btnShibie.Location = new Point(826, 666);
+            btnShibie.Location = new Point(128, 640);
             btnShibie.Name = "btnShibie";
             btnShibie.Size = new Size(75, 31);
             btnShibie.TabIndex = 10;
@@ -94,29 +93,94 @@
             // chkBox1
             // 
             chkBox1.AutoSize = true;
-            chkBox1.Location = new Point(745, 672);
+            chkBox1.Location = new Point(47, 646);
             chkBox1.Name = "chkBox1";
             chkBox1.Size = new Size(75, 21);
             chkBox1.TabIndex = 11;
             chkBox1.Text = "是否换行";
             chkBox1.UseVisualStyleBackColor = true;
             // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(585, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(939, 707);
+            tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(txtContent);
+            tabPage1.Controls.Add(chkBox1);
+            tabPage1.Controls.Add(btnShibie);
+            tabPage1.Location = new Point(4, 26);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(931, 677);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "图片识别文本";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btnSaveHtml);
+            tabPage2.Controls.Add(btnHtmlReg);
+            tabPage2.Controls.Add(txtHtmlContent);
+            tabPage2.Location = new Point(4, 26);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(931, 677);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "图片识别为Html";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnHtmlReg
+            // 
+            btnHtmlReg.Location = new Point(40, 638);
+            btnHtmlReg.Name = "btnHtmlReg";
+            btnHtmlReg.Size = new Size(85, 33);
+            btnHtmlReg.TabIndex = 1;
+            btnHtmlReg.Text = "识别";
+            btnHtmlReg.UseVisualStyleBackColor = true;
+            btnHtmlReg.Click += btnHtmlReg_Click;
+            // 
+            // txtHtmlContent
+            // 
+            txtHtmlContent.Location = new Point(6, 6);
+            txtHtmlContent.Multiline = true;
+            txtHtmlContent.Name = "txtHtmlContent";
+            txtHtmlContent.Size = new Size(919, 626);
+            txtHtmlContent.TabIndex = 0;
+            // 
+            // btnSaveHtml
+            // 
+            btnSaveHtml.Location = new Point(148, 638);
+            btnSaveHtml.Name = "btnSaveHtml";
+            btnSaveHtml.Size = new Size(75, 31);
+            btnSaveHtml.TabIndex = 2;
+            btnSaveHtml.Text = "保存文件";
+            btnSaveHtml.UseVisualStyleBackColor = true;
+            btnSaveHtml.Click += btnSaveHtml_Click;
+            // 
             // 图片识别工具
             // 
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1424, 709);
-            Controls.Add(chkBox1);
-            Controls.Add(btnShibie);
-            Controls.Add(label3);
+            ClientSize = new Size(1536, 719);
+            Controls.Add(tabControl1);
             Controls.Add(btnSelImage);
             Controls.Add(picBox);
-            Controls.Add(txtContent);
             Name = "图片识别工具";
             Text = "图片识别工具(内部)";
             ((System.ComponentModel.ISupportInitialize)picBox).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -126,8 +190,13 @@
         private PictureBox picBox;
         private Button btnSelImage;
         private OpenFileDialog openFileDialog1;
-        private Label label3;
         private Button btnShibie;
         private CheckBox chkBox1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox txtHtmlContent;
+        private Button btnHtmlReg;
+        private Button btnSaveHtml;
     }
 }
